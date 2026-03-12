51669a40c6cc8e3f561e1844652035e73046f755
Nimde3deelux <adamsehili28@gmail.com>
2026-03-12 16:50:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function scrollContact()
-{
-document.getElementById(contact).scrollIntoView({
-behaviorsmooth
-});
+const cursor = document.querySelector(".cursor")
+
+document.addEventListener("mousemove", e=>{
+cursor.style.left = e.clientX + "px"
+cursor.style.top = e.clientY + "px"
+})
+
+function scrollContact(){
+document.getElementById("contact").scrollIntoView({
+behavior:"smooth"
+})
 }
